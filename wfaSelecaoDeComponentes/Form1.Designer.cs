c34f7688ad1a3c2e4da5ffdecffa889cdf212bf9
José Júnior <junior_dariana@hotmail.com>
2017-01-30 23:32:26 -0200

    first commit

---
+++ wfaSelecaoDeComponentes/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace wfaSelecaoDeComponentes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cb4 = new System.Windows.Forms.ComboBox();
+            this.cb3 = new System.Windows.Forms.ComboBox();
+            this.cb2 = new System.Windows.Forms.ComboBox();
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.btnComponentes = new System.Windows.Forms.Button();
+            this.btnMonitor = new System.Windows.Forms.Button();
+            this.btnMultimidia = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnMulti2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cb4);
+            this.groupBox1.Controls.Add(this.cb3);
+            this.groupBox1.Controls.Add(this.cb2);
+            this.groupBox1.Controls.Add(this.cb1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(175, 202);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Componentes";
+            // 
+            // cb4
+            // 
+            this.cb4.FormattingEnabled = true;
+            this.cb4.Items.AddRange(new object[] {
+            "HD",
+            "Placa de Video",
+            "Memoria",
+            "Processador",
+            "Placa de Som",
+            "Placa de Rede"});
+            this.cb4.Location = new System.Drawing.Point(25, 162);
+            this.cb4.Name = "cb4";
+            this.cb4.Size = new System.Drawing.Size(121, 21);
+            this.cb4.TabIndex = 3;
+            // 
+            // cb3
+            // 
+            this.cb3.FormattingEnabled = true;
+            this.cb3.Items.AddRange(new object[] {
+            "HD",
+            "Placa de Video",
+            "Memoria",
+            "Processador",
+            "Placa de Som",
+            "Placa de Rede"});
+            this.cb3.Location = new System.Drawing.Point(25, 120);
+            this.cb3.Name = "cb3";
+            this.cb3.Size = new System.Drawing.Size(121, 21);
+            this.cb3.TabIndex = 3;
+            // 
+            // cb2
+            // 
+            this.cb2.FormattingEnabled = true;
+            this.cb2.Items.AddRange(new object[] {
+            "HD",
+            "Placa de Video",
+            "Memoria",
+            "Processador",
+            "Placa de Som",
+            "Placa de Rede"});
+            this.cb2.Location = new System.Drawing.Point(25, 78);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(121, 21);
+            this.cb2.TabIndex = 3;
+            // 
+            // cb1
+            // 
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Items.AddRange(new object[] {
+            "HD",
+            "Placa de Video",
+            "Memoria",
+            "Processador",
+            "Placa de Som",
+            "Placa de Rede"});
+            this.cb1.Location = new System.Drawing.Point(25, 36);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(121, 21);
+            this.cb1.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(193, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(152, 202);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Monitor";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(29, 91);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(46, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "LED";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(29, 40);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(42, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Flat";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkedListBox1);
+            this.groupBox3.Location = new System.Drawing.Point(356, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 202);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Multimídia";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Caixad de Som",
+            "Microfone",
+            "Headset",
+            "WebCam",
+            "JoyStick",
+            "CD-Rom",
+            "Teclado",
+            "Monitor",
+            "Mouse",
+            "Home Theater",
+            "Caixad de Som",
+            "Microfone",
+            "Headset",
+            "WebCam",
+            "JoyStick",
+            "CD-Rom",
+            "Teclado",
+            "Monitor",
+            "Mouse",
+            "Home Theater"});
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 27);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(188, 169);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // btnComponentes
+            // 
+            this.btnComponentes.Location = new System.Drawing.Point(59, 220);
+            this.btnComponentes.Name = "btnComponentes";
+            this.btnComponentes.Size = new System.Drawing.Size(83, 39);
+            this.btnComponentes.TabIndex = 3;
+            this.btnComponentes.Text = "Mostrar Valores";
+            this.btnComponentes.UseVisualStyleBackColor = true;
+            this.btnComponentes.Click += new System.EventHandler(this.btnComponentes_Click);
+            // 
+            // btnMonitor
+            // 
+            this.btnMonitor.Location = new System.Drawing.Point(222, 220);
+            this.btnMonitor.Name = "btnMonitor";
+            this.btnMonitor.Size = new System.Drawing.Size(83, 39);
+            this.btnMonitor.TabIndex = 4;
+            this.btnMonitor.Text = "Mostrar Valores";
+            this.btnMonitor.UseVisualStyleBackColor = true;
+            this.btnMonitor.Click += new System.EventHandler(this.btnMonitor_Click);
+            // 
+            // btnMultimidia
+            // 
+            this.btnMultimidia.Location = new System.Drawing.Point(419, 220);
+            this.btnMultimidia.Name = "btnMultimidia";
+            this.btnMultimidia.Size = new System.Drawing.Size(83, 39);
+            this.btnMultimidia.TabIndex = 5;
+            this.btnMultimidia.Text = "Mostrar Valores";
+            this.btnMultimidia.UseVisualStyleBackColor = true;
+            this.btnMultimidia.Click += new System.EventHandler(this.btnMultimidia_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listView1);
+            this.groupBox4.Location = new System.Drawing.Point(12, 265);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 133);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Multimidia2";
+            // 
+            // listView1
+            // 
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.listView1.Location = new System.Drawing.Point(3, 16);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(191, 111);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // btnMulti2
+            // 
+            this.btnMulti2.Location = new System.Drawing.Point(222, 318);
+            this.btnMulti2.Name = "btnMulti2";
+            this.btnMulti2.Size = new System.Drawing.Size(83, 39);
+            this.btnMulti2.TabIndex = 7;
+            this.btnMulti2.Text = "Mostrar Valores";
+            this.btnMulti2.UseVisualStyleBackColor = true;
+            this.btnMulti2.Click += new System.EventHandler(this.btnMulti2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(356, 281);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 111);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 410);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnMulti2);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.btnMultimidia);
+            this.Controls.Add(this.btnMonitor);
+            this.Controls.Add(this.btnComponentes);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cb4;
+        private System.Windows.Forms.ComboBox cb3;
+        private System.Windows.Forms.ComboBox cb2;
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button btnComponentes;
+        private System.Windows.Forms.Button btnMonitor;
+        private System.Windows.Forms.Button btnMultimidia;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnMulti2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ wfaSelecaoDeComponentes/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfaSelecaoDeComponentes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMonitor_Click(object sender, EventArgs e)
+        {
+            if (this.radioButton1.Checked)
+            {
+                MessageBox.Show("Voçê escolheu um monitor Flat");
+                listView1.Items.Add(this.radioButton1.Text);
+            }
+            if (this.radioButton2.Checked)
+            {
+                MessageBox.Show("Voçê escolheu um monitor normal");
+                listView1.Items.Add(this.radioButton2.Text);
+            }
+
+            
+        }
+
+        private void btnComponentes_Click(object sender, EventArgs e)
+        {
+            if(cb1.SelectedIndex != -1)
+            {
+                listView1.Items.Add(this.cb1.SelectedItem.ToString());
+            }
+            if (cb2.SelectedIndex != -1)
+            {
+                listView1.Items.Add(this.cb2.SelectedItem.ToString());
+            }
+            if (cb3.SelectedIndex != -1)
+            {
+                listView1.Items.Add(this.cb3.SelectedItem.ToString());
+            }
+            if (cb4.SelectedIndex != -1)
+            {
+                listView1.Items.Add(this.cb4.SelectedItem.ToString());
+            }
+        }
+
+        private void btnMultimidia_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.checkedListBox1.Items.Count; i++)
+            {
+                if (this.checkedListBox1.GetSelected(i))
+                {
+                    listView1.Items.Add(this.checkedListBox1.SelectedItem.ToString());
+                }
+            }
+        }
+
+        private void btnMulti2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.listView1.Items.Count; i++)
+            {
+                textBox1.Text = listView1.Items[i].ToString();
+            }
+        }
+    }
+}
